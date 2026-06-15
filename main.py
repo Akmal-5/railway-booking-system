e242ea16354438d53e5bd030f9401151dd361c2f
Akmal <amirokhanov5@gmail.com>
2026-06-15 21:57:15 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+from app.db.create_db import create_tables
+from app.db.config import AsyncSessionmaker
+from app.db.data_manipulation import create_trains , create_seats , create_stations , create_train_routes
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
 
+@app.on_event("startup")
+async def startup () :
+    await create_tables()
+    
+    async with AsyncSessionmaker() as session :
+        trains = await create_trains(session)
+        stations = await create_stations(session)
+        
+        for train in trains :
+            await create_seats(session , train.id)
+            await create_train_routes(session , train.id , stations)
+    
 if __name__ == "__main__" :
-    pass
+    uvicorn.run("main:app" , reload=True)    
